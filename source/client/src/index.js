054839642d32fc43e8c37b780b8db901bb57ec3e
Habi Sandurov <habi.alonso98@gmail.com>
2020-02-07 17:42:19 +0600

    refactoring code

--- source/client/src/index.js
+++ source/client/src/index.js
@@ -2,10 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'connected-react-router';
+import store, {history} from './store/configureStore';
+
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+
+const app = (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+             <App/>
+        </ConnectedRouter>
+    </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
